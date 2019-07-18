4ce85088c2b797d897b441673484d75334863248
LiuYan <luckyyam@yeah.net>
2019-07-18 22:26:25 +0800

    刘岩

--- takeout/src/main/java/com/hellojava/entity/BusinessType.java
+++ takeout/src/main/java/com/hellojava/entity/BusinessType.java
@@ -13,15 +13,6 @@ public class BusinessType implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "bus_type_id")
     private int typeId;
-    @Column(name = "bus_type_food")
-    private String food;
-    @Column(name = "bus_type_dessdrink")
-    private String dessDrink;
-    @Column(name = "bus_type_market")
-    private String market;
-    @Column(name = "bus_type_vegfruit")
-    private String vegFruit;
-    @Column(name = "bus_type_medicine")
-    private String medicine;
-
+    @Column(name = "bus_type_name")
+    private String typeName;
 }
